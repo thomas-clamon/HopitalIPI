7cfccb6c8beb54d77e97068acda053842a4633e0
thomas clamon <thomas.clamon@gmail.com>
2022-12-14 13:27:47 +0100

    add test imput

--- src/main/java/com/ipi/hopital/Ressources.java
+++ src/main/java/com/ipi/hopital/Ressources.java
@@ -1,2 +1,28 @@
-package com.ipi.hopital;public class Ressources {
+package com.ipi.hopital;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("exemple")
+public class Ressources {
+
+    @GetMapping("hello")
+    public String helloWorld(){
+        return "Bonjour a tous";
+    }
+
+     @GetMapping("bonjour/{nom}")
+    public String exemple(@PathVariable String nom){
+        return "Bonjour " + nom;
+     }
+
+     @GetMapping("hello2")
+     public String test(@RequestParam String prenom, @RequestParam String nom){
+        return String.format("je m'appelle %s %s", nom, prenom);
+     }
+
+     @GetMapping("test")
+    public int exemple(){
+        return 1+1;
+     }
 }
