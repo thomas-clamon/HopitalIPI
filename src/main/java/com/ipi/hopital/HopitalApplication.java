8a52810876630e47aeee403fd4166908a319e214
thomas clamon <thomas.clamon@gmail.com>
2022-12-14 12:05:17 +0100

    Initial commit

---
+++ src/main/java/com/ipi/hopital/HopitalApplication.java
@@ -0,0 +1,13 @@
+package com.ipi.hopital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HopitalApplication.class, args);
+    }
+
+}
